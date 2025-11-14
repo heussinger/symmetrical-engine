abf304c8348721e7ace18ff45985101838340117
heussinger <heussinger@xplace.de>
2025-11-14 09:25:42 +0100

    Update SoftwareEngineerRepository.java

--- src/main/java/com/example/demo/SoftwareEngineerRepository.java
+++ src/main/java/com/example/demo/SoftwareEngineerRepository.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
     
 }
