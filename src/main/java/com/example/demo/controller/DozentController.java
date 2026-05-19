8e881345612d8054b198a4ea94941172696df2be
heussinger <heussinger@xplace.de>
2026-05-19 11:04:37 +0200

    Add Dozent entity import to DozentController

--- src/main/java/com/example/demo/controller/DozentController.java
+++ src/main/java/com/example/demo/controller/DozentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Dozent;
 import com.example.demo.service.DozentService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
